84b1c6ef060dcdcdf913603a2d4750d2e7e53baf
HarshGupta <harshgupta88911@gmail.com>
2024-11-07 16:07:56 +0530

    remove: app function function from firebase config

--- ezzyshop/src/lib/firebase.tsx
+++ ezzyshop/src/lib/firebase.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import { useEffect } from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB7As2ZzOIA98XihRH1r0w_0DKfjMuI-ks",
@@ -16,34 +15,9 @@ const firebaseConfig = {
   measurementId: "G-PBN0LF0H9W",
 };
 
-// Initialize Firebase app and services (conditionally for client-side)
-let app, auth, provider, database, storage;
-
-if (typeof window !== "undefined") {
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  provider = new GoogleAuthProvider();
-  database = getFirestore(app);
-  storage = getStorage(app);
-}
-
-const App = () => {
-  useEffect(() => {
-    const initializeFirebaseAnalytics = async () => {
-      const app = initializeApp(firebaseConfig);
-      if (await isSupported()) {
-        const analytics = getAnalytics(app);
-        console.log("Analytics initialized", analytics);
-      } else {
-        console.log("Firebase Analytics is not supported in this environment");
-      }
-    };
-
-    initializeFirebaseAnalytics();
-  }, []);
-
-  // return <div>Your app content</div>;/
-};
-
-export { auth, provider, database, storage };
-export default App;
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const provider = new GoogleAuthProvider();
+export const auth = getAuth();
+export const database = getFirestore(app);
+export const storage = getStorage();
