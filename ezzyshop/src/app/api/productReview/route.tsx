734d62ef1475d21da48c880c68ca1c2b47806652
HarshGupta <harshgupta88911@gmail.com>
2024-11-14 23:25:17 +0530

    added: concept of fetching reviews of products

---
+++ ezzyshop/src/app/api/productReview/route.tsx
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import amazon from "amazon-buddy";
+
+// This is your route handler for the API endpoint.
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const searchID = searchParams.get("searchID");
+
+  // Debugging: Log the searchID to ensure it's coming through correctly
+  console.log("Search ID:", searchID);
+
+  // Ensure a search term (ASIN) is provided
+  if (!searchID) {
+    return NextResponse.json(
+      { error: "Please provide a search term." },
+      { status: 400 },
+    );
+  }
+
+  try {
+    // Fetch product details using the amazon-buddy library
+    const reviews = await amazon.reviews({
+      asin: searchID,
+      number: 50,
+      rating: [1, 5],
+      country: "IN",
+    });
+    return NextResponse.json(reviews);
+  } catch (error) {
+    console.error("Error fetching Amazon data:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch data from Amazon." },
+      { status: 500 },
+    );
+  }
+}
