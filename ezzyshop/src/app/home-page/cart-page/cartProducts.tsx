0909e04fc49a434c431bc17c9b8b96215320edcc
HarshGupta <harshgupta88911@gmail.com>
2024-11-14 23:26:59 +0530

    added: a page for adding the concept of addingn product into cart

--- ezzyshop/src/app/home-page/cart-page/cartProducts.tsx
+++ ezzyshop/src/app/home-page/cart-page/cartProducts.tsx
@@ -1,76 +1,77 @@
-import info from "@/assets/icon/info.png";
-import StarRating from "@/components/StartRating/startRating";
-import Image from "next/image";
+// import info from "@/assets/icon/info.png";
+// import StarRating from "@/components/StartRating/startRating";
+// import Image from "next/image";
 
-interface ProductsItemType {
-  products: Product; // Single Product, not an array
-}
+// interface ProductsItemType {
+//   products: Product; // Single Product, not an array
+// }
 
-interface PriceDetails {
-  before_price: number;
-  current_price: number;
-  savings_amount: number;
-}
+// interface PriceDetails {
+//   before_price: number;
+//   current_price: number;
+//   savings_amount: number;
+// }
 
-interface Product {
-  title: string;
-  thumbnail: string;
-  reviews: {
-    rating: number;
-  };
-  price: PriceDetails;
-}
+// interface Product {
+//   title: string;
+//   main_image: string;
+//   reviews: {
+//     rating: number;
+//   };
+//   price: PriceDetails;
+// }
 
-const CartProducts: React.FC<ProductsItemType> = ({
-  products,
-}: ProductsItemType) => {
-  const { title, thumbnail, reviews, price } = products;
-  const { before_price, current_price, savings_amount } = price;
+// const CartProducts: React.FC<ProductsItemType> = ({
+//   products,
+// }: ProductsItemType) => {
+//   const { title, main_image, reviews, price } = products;
+//   const { before_price, current_price, savings_amount } = price;
 
-  const discountPercentage = before_price
-    ? ((before_price - current_price) / before_price) * 100
-    : 0;
+//   const discountPercentage = before_price
+//     ? ((before_price - current_price) / before_price) * 100
+//     : 0;
 
-  const discount =
-    isNaN(discountPercentage) || !isFinite(discountPercentage)
-      ? 0
-      : discountPercentage;
+//   const discount =
+//     isNaN(discountPercentage) || !isFinite(discountPercentage)
+//       ? 0
+//       : discountPercentage;
+//   console.log(products);
 
-  return (
-    <div className="flex h-52 w-full gap-5">
-      <div className="flex h-52 w-52 items-center justify-center bg-[#ffffff] object-cover">
-        <Image
-          src={thumbnail}
-          alt={`${title} image`}
-          className="productImage"
-          width={150}
-          height={100}
-          priority
-        />
-      </div>
-      <div className="flex flex-col gap-3 p-3">
-        <div className="flex flex-col gap-2">
-          <div className="flex gap-2">
-            <span className="text-sm">Sponsored</span>
-            <Image src={info} alt="info" className="h-5 w-5" />
-          </div>
-          <span className="font-semibold">{title}</span>
-          <StarRating defaultRating={reviews.rating} size={20} maxRating={5} />
-          <div className="flex items-center gap-3">
-            <span className="text-3xl font-bold">{`Rs ${current_price}`}</span>
-            {before_price !== 0 && (
-              <div className="flex items-center justify-center gap-1">
-                <span className="line-through">{`M.R.P: Rs ${before_price}`}</span>
-              </div>
-            )}
-            <span className="font-semibold">
-              {discount !== 0 && `(${Math.trunc(discount)}% off)`}
-            </span>
-          </div>
-          {savings_amount !== 0 && <span>{`Save Rs ${savings_amount}`}</span>}
-        </div>
-      </div>
-    </div>
-  );
-};
-export default CartProducts;
+//   return (
+//     <div className="flex h-52 w-full gap-5">
+//       <div className="flex h-52 w-52 items-center justify-center bg-[#ffffff] object-cover">
+//         <Image
+//           src={main_image}
+//           alt={`${title} image`}
+//           className="productImage"
+//           width={150}
+//           height={100}
+//           priority
+//         />
+//       </div>
+//       <div className="flex flex-col gap-3 p-3">
+//         <div className="flex flex-col gap-2">
+//           <div className="flex gap-2">
+//             <span className="text-sm">Sponsored</span>
+//             <Image src={info} alt="info" className="h-5 w-5" />
+//           </div>
+//           <span className="font-semibold">{title}</span>
+//           <StarRating defaultRating={reviews.rating} size={20} maxRating={5} />
+//           <div className="flex items-center gap-3">
+//             <span className="text-3xl font-bold">{`Rs ${current_price}`}</span>
+//             {before_price !== 0 && (
+//               <div className="flex items-center justify-center gap-1">
+//                 <span className="line-through">{`M.R.P: Rs ${before_price}`}</span>
+//               </div>
+//             )}
+//             <span className="font-semibold">
+//               {discount !== 0 && `(${Math.trunc(discount)}% off)`}
+//             </span>
+//           </div>
+//           {savings_amount !== 0 && <span>{`Save Rs ${savings_amount}`}</span>}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default CartProducts;
--- ezzyshop/src/app/home-page/cart-page/page.tsx
+++ ezzyshop/src/app/home-page/cart-page/page.tsx
@@ -1,72 +1,55 @@
-"use client";
-import { useEffect, useState } from "react";
-import CartProducts from "./cartProducts";
-import { useLoading } from "@/app/loadingContext";
-import { toast } from "react-toastify";
+// "use client";
+// // import { useEffect, useState } from "react";
+// // import { useLoading } from "@/app/loadingContext";
+// // import { toast } from "react-toastify";
+// // import { database } from "@/lib/firebase";
+// // import { collection, getDocs } from "firebase/firestore";
+// import CartProducts from "./cartProducts";
 
-interface PriceDetails {
-  before_price: number;
-  current_price: number;
-  savings_amount: number;
-}
+// interface PriceDetails {
+//   before_price: number;
+//   current_price: number;
+//   savings_amount: number;
+// }
 
-interface Product {
-  asin: string;
-  title: string;
-  thumbnail: string;
-  reviews: {
-    rating: number;
-  };
-  price: PriceDetails;
-  length: number;
-}
+// interface Product {
+//   asin: string;
+//   title: string;
+//   thumbnail: string;
+//   reviews: {
+//     rating: number;
+//   };
+//   price: PriceDetails;
+//   length: number;
+// }
 
-interface ProductsResponse {
-  result: Product[];
-}
+// interface ProductsResponse {
+//   result: Product[];
+// }
 
-function CartPage() {
-  const [products, setProducts] = useState<ProductsResponse | null>(null);
-  const { setLoading } = useLoading();
+// function CartPage() {
+//   // useEffect(() => {
+//   //   const storedProducts = localStorage.getItem("productDetails");
+//   //   if (storedProducts) {
+//   //     setCartItems(JSON.parse(storedProducts));
+//   //   }
+//   // }, []);
 
-  useEffect(
-    function () {
-      async function getData() {
-        try {
-          setLoading(true);
-          const res = await fetch("/api/amazon?searchTerm=iphone");
-          const data = await res.json();
-          setProducts(data);
-        } catch (err) {
-          if (err instanceof Error) {
-            toast.error(err.message);
-            console.log(err);
-          }
-        } finally {
-          setLoading(false);
-        }
-      }
+//   // const items = cartItems.result || [];
 
-      getData();
-    },
-    [setLoading],
-  );
-
-  const items = products?.result || [];
-
-  return (
-    <div className="h-screen w-screen bg-[#FEE5E9] p-2">
-      <div className="p-3">
-        <span className="text-xl font-semibold">Shopping Cart</span>
-      </div>
-      <div className="flex h-screen w-screen flex-col items-center bg-[#FEE5E9]">
-        <div className="flex h-[75%] w-[95%] flex-col items-center gap-24 overflow-scroll overflow-x-hidden bg-white p-4">
-          {items.map((item) => (
-            <CartProducts products={item} key={item.asin} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-}
-export default CartPage;
+//   return (
+//     <div className="h-screen w-screen bg-[#FEE5E9] p-2">
+//       <div className="p-3">
+//         <span className="text-xl font-semibold">Shopping Cart</span>
+//       </div>
+//       <div className="flex h-screen w-screen flex-col items-center bg-[#FEE5E9]">
+//         <div className="flex h-[75%] w-[95%] flex-col items-center gap-24 overflow-scroll overflow-x-hidden bg-white p-4">
+//           {items.map((item) => (
+//             <CartProducts products={item} key={item.asin} />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+// export default CartPage;
