4684054894b862fbcd41784cb29aaff0d1cbe1aa
HarshGupta <harshgupta88911@gmail.com>
2024-11-15 23:44:53 +0530

    added: product types

---
+++ ezzyshop/src/types/productTypes.tsx
@@ -0,0 +1,21 @@
+// types/ProductTypes.ts
+export interface PriceDetails {
+  before_price: number;
+  current_price: number;
+  savings_amount: number;
+  savings_percent: number;
+}
+
+export interface Product {
+  asin: string;
+  title: string;
+  thumbnail: string;
+  reviews: {
+    rating: number;
+  };
+  price: PriceDetails;
+  badges?: string[]; // Optional
+  feature_bullets?: string[]; // Optional
+  images?: string[]; // Optional
+  description?: string; // Optional
+}
