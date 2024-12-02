9217c7b50b4edbb81388741e88702894aa4182ff
HarshGupta <harshgupta88911@gmail.com>
2024-12-02 21:04:45 +0530

    added: a order page for completed order

---
+++ ezzyshop/src/app/home-page/order-page/orderDetail.tsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+import { TransformComponent } from "react-zoom-pan-pinch";
+import React from "react";
+import { TransformWrapper } from "react-zoom-pan-pinch";
+
+export type OrderProduct = {
+  order: {
+    amount: number;
+    currency: string;
+    order_id: string;
+    payment_id: string;
+    products: {
+      before_price: number;
+      price: number;
+      quantity: number;
+      quantityBeforePrice: number;
+      quantityPrice: number;
+      quantitySavingPrice: number;
+      savings_amount: number;
+      savings_percent: number;
+      thumbnail: string;
+      title: string;
+    };
+    status: string;
+    user: {
+      email: string;
+      name: string;
+    };
+  };
+};
+
+const OrderDetail: React.FC<OrderProduct> = ({ order }) => {
+  const { products, status } = order;
+  const {
+    thumbnail,
+    title,
+    before_price,
+    price,
+    quantity,
+    quantityBeforePrice,
+    quantitySavingPrice,
+    savings_amount,
+    savings_percent,
+  } = products;
+
+  return (
+    <div className="flex h-auto w-full items-center gap-2 p-2">
+      <div className="flex h-full w-60 items-center justify-center rounded-xl bg-white object-cover p-2">
+        <TransformWrapper>
+          <TransformComponent>
+            <Image
+              src={thumbnail}
+              alt={`${title} image`}
+              className="productImage rounded-sm"
+              width={150}
+              height={100}
+              priority
+            />
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
+      <div className="flex flex-col gap-3 p-3">
+        <div className="flex flex-col gap-3 text-left">
+          <span className="font-semibold">{title}</span>
+          <div className="flex items-center gap-3">
+            <span className="text-3xl font-bold">
+              {quantity === 1 && ` Rs ${price}`}
+            </span>
+            {before_price !== 0 && (
+              <div className="flex items-center justify-center gap-1">
+                <span className="line-through">
+                  {quantity === 1
+                    ? `M.R.P: Rs ${Math.trunc(before_price)}`
+                    : `M.R.P Rs ${Math.trunc(quantityBeforePrice)}`}
+                </span>
+              </div>
+            )}
+            <span className="font-semibold">
+              {savings_percent !== 0 && `(${Math.trunc(savings_percent)}% off)`}
+            </span>
+          </div>
+          {savings_amount !== 0 && (
+            <span>
+              {quantity === 1
+                ? `Save Rs ${savings_amount}`
+                : `Save Rs ${quantitySavingPrice}`}
+            </span>
+          )}
+        </div>
+        <div>{`✅ Order ${status}`}</div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetail;
---
+++ ezzyshop/src/app/home-page/order-page/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { database } from "@/lib/firebase";
+import { useLoading } from "@/app/loadingContext";
+import OrderDetail from "./orderDetail";
+
+type OrderProduct = {
+  amount: number;
+  currency: string;
+  order_id: string;
+  payment_id: string;
+  products: {
+    before_price: number;
+    price: number;
+    quantity: number;
+    quantityBeforePrice: number;
+    quantityPrice: number;
+    quantitySavingPrice: number;
+    savings_amount: number;
+    savings_percent: number;
+    thumbnail: string;
+    title: string;
+  };
+  status: string;
+  user: {
+    email: string;
+    name: string;
+  };
+};
+
+function OrderPage() {
+  const [orderItems, setOrderItems] = useState<OrderProduct[]>([]);
+  const { setLoading } = useLoading();
+
+  useEffect(() => {
+    async function fetchCartProducts() {
+      try {
+        setLoading(true); // Indicate loading state
+        const cartCollection = collection(database, "orders");
+        const cartSnapshot = await getDocs(cartCollection);
+
+        const products = cartSnapshot.docs.map((doc) => {
+          const data = doc.data();
+          return {
+            amount: data.amount || 0,
+            currency: data.currency || "INR",
+            order_id: data.order_id || "",
+            payment_id: data.payment_id || "",
+            products: {
+              before_price: data.products?.before_price || 0,
+              price: data.products?.price || 0,
+              quantity: data.products?.quantity || 0,
+              quantityBeforePrice: data.products?.quantityBeforePrice || 0,
+              quantityPrice: data.products?.quantityPrice || 0,
+              quantitySavingPrice: data.products?.quantitySavingPrice || 0,
+              savings_amount: data.products?.savings_amount || 0,
+              savings_percent: data.products?.savings_percent || 0,
+              thumbnail: data.products?.thumbnail || "",
+              title: data.products?.title || "",
+            },
+            status: data.status || "Pending",
+            user: {
+              email: data.user?.email || "",
+              name: data.user?.name || "",
+            },
+          } as OrderProduct;
+        });
+
+        setOrderItems(products);
+      } catch (error) {
+        console.error("Error fetching Order data:", error);
+      } finally {
+        setLoading(false); // End loading state
+      }
+    }
+
+    fetchCartProducts();
+  }, [setLoading]);
+
+  //   console.log(orderItems);
+
+  return (
+    <div className="relative top-48 flex h-screen w-screen flex-col gap-5 overflow-scroll bg-[#FEE5E9] pb-40 sm:relative sm:top-0 sm:pb-20">
+      <div className="p-3">
+        <span className="text-xl font-semibold">Order Details</span>
+      </div>
+      <div className="flex h-screen w-screen flex-col items-center gap-5 bg-[#FEE5E9]">
+        {orderItems.map((item) => (
+          <OrderDetail order={item} key={item.order_id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default OrderPage;
