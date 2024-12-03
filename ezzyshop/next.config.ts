0d091010c615bc2257fc4af8c677482b30fc93ca
HarshGupta <harshgupta88911@gmail.com>
2024-12-03 10:39:55 +0530

    update: name of key and secret key

--- ezzyshop/next.config.ts
+++ ezzyshop/next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
     domains: ["www.amazon.in", "m.media-amazon.com"],
   },
   env: {
-    RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
-    RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
+    RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+    RAZORPAY_KEY_SECRET: process.env.NEXT_PUBLIC_RAZORPAY_KEY_SECRET,
   },
 };
 
--- ezzyshop/src/app/api/createOrder/route.ts
+++ ezzyshop/src/app/api/createOrder/route.ts
@@ -1,13 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
-if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+if (
+  !process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID ||
+  !process.env.NEXT_PUBLIC_RAZORPAY_KEY_SECRET
+) {
   throw new Error("Razorpay key_id and key_secret are required");
 }
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID as string,
-  key_secret: process.env.RAZORPAY_KEY_SECRET as string,
+  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID as string,
+  key_secret: process.env.NEXT_PUBLIC_RAZORPAY_KEY_SECRET as string,
 });
 
 export async function POST(req: NextRequest) {
