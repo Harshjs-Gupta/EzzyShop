cdfacc6ff34f3903f1caf72de2f6351cf50bb38f
HarshGupta <harshgupta88911@gmail.com>
2024-12-03 10:48:00 +0530

    added: key

--- ezzyshop/next.config.ts
+++ ezzyshop/next.config.ts
@@ -5,8 +5,9 @@ const nextConfig: NextConfig = {
     domains: ["www.amazon.in", "m.media-amazon.com"],
   },
   env: {
-    RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-    RAZORPAY_KEY_SECRET: process.env.NEXT_PUBLIC_RAZORPAY_KEY_SECRET,
+    NEXT_PUBLIC_RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+    NEXT_PUBLIC_RAZORPAY_KEY_SECRET:
+      process.env.NEXT_PUBLIC_RAZORPAY_KEY_SECRET,
   },
 };
 
