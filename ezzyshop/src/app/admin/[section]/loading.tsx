d94505b94fd595db96eea269705f46db1c89f9c4
Harsh Gupta <harshgupta88911@gmail.com>
2026-06-07 17:55:14 +0530

    added: loading page for admina panel

---
+++ ezzyshop/src/app/admin/[section]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex h-screen items-center justify-center text-2xl text-[#3f3f3f]">
+      loading...
+    </div>
+  );
+};
+
+export default loading;
