ebd84f9a9f61c85ef651ef98b7935f0a028cfad7
Harsh Gupta <harshgupta88911@gmail.com>
2026-06-07 17:59:25 +0530

    update: theme of slug page

--- ezzyshop/src/app/home-page/[itemsSlug]/page.tsx
+++ ezzyshop/src/app/home-page/[itemsSlug]/page.tsx
@@ -1,11 +1,10 @@
 import SlugProduct from "@/components/slugPageComponent/slugProducts";
 import style from "./slugPage.module.css";
-import Navbar from "@/components/home-page-component/navbar";
 
 function SlugPage() {
   return (
     <>
-      <Navbar />
+
       <div
         className={`${style.background} sm: relative top-48 z-0 flex h-screen w-screen flex-col overflow-scroll pb-72 sm:relative sm:top-0 sm:p-2 sm:pb-10`}
       >
